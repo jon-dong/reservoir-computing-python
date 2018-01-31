1abc585ef51576cbea5120704182d618ed9f08ee
Jonathan Dong <jonathan.dong@ens.fr>
2018-01-31 14:35:11 +0100

    Add new timeseries datasets
    Mackey-Glass and Multiple Sinewave Oscillator

--- data.py
+++ data.py
@@ -14,9 +14,6 @@ Typical parameters:
 Output:
     - input_data: input sequence, np array (sequence_length, 1)
     - y: output sequence, np array (sequence_length, 1)
-
-To-do:
-    - Add other time series from the Matlab Code
 """
 
 import numpy as np
@@ -32,7 +29,7 @@ def memory(sequence_length=1000, memory_delay=1, random_state=None):
     return input_data, y
 
 
-def xor(sequence_length=100, memory_delay=1, random_state=None):
+def xor(sequence_length=1000, memory_delay=1, random_state=None):
     random_state = check_random_state(random_state)
     input_data = random_state.randint(low=0, high=2, size=(sequence_length, 1))
 
@@ -54,3 +51,39 @@ def narma(sequence_length=1000, random_state=None):
             1.5 * input_data[i-10] * input_data[i-1] + 0.1
 
     return np.tanh(input_data), np.tanh(y)
+
+
+def mackey_glass(sequence_length=1000, tau=17, random_state=None):    
+    random_state = check_random_state(random_state)
+    beta = 0.2;
+    gamma = 0.1;
+    tau = 17;
+    n = 10;
+    h = 1;
+    memory_length = int(tau/h)
+
+    input_data = np.zeros((sequence_length, 1))
+    input_data[0:memory_length] = 1.1 + 0.2 * random_state.normal(loc=0., scale=1, size=(memory_length, 1))
+    for iSequence in range(memory_length, sequence_length):
+        input_data[iSequence] = (1 - h * gamma) * input_data[iSequence - 1] + \
+        beta * h * input_data[iSequence - memory_length] / \
+        (1 + input_data[iSequence - memory_length] ** n);
+
+    input_data = np.tanh(input_data - 1)
+    y = np.roll(input_data, -1)
+    return input_data, y
+
+
+def mso(sequence_length=1000, random_state=None):
+    '''
+    Generate the Multiple Sinewave Oscillator time-series, a sum of two sines
+    with incommensurable periods. Parameters are:
+    '''
+    random_state = check_random_state(random_state)
+    phase = np.random.rand()
+    x = np.atleast_2d(np.arange(sequence_length)).T
+    input_data = np.sin(0.2 * x + phase) + np.sin(0.311 * x + phase)
+
+    y = np.roll(input_data, -1)
+    return input_data, y
+
