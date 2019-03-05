3390c01bfd9361ac38dea286de45bbaa7b666baa
Jonathan Dong <jonathan.dong@ens.fr>
2019-03-05 18:01:52 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit c7512cf0aacb26ca1bd7c687e7c6c32860dbb178.

--- encode.py
+++ encode.py
@@ -30,21 +30,17 @@ def naive_binary(mat, lower_bound=-0.5, higher_bound=0.5, binary_dim=10):
     enc_input_data = np.repeat(np.zeros(mat.shape), binary_dim, axis=-1)
     for i_binary in range(binary_dim):
         enc_input_data[..., i_binary::binary_dim] = mat > lower_bound + step * i_binary
-    
     return enc_input_data
 
 
-def local_binary(mat, lower_bound=-0.5, higher_bound=0.5, step=0.5, binary_dim=10, enc_init=False, step_vec=None, dither_vec=None):
+def local_binary(mat, lower_bound=-0.5, higher_bound=0.5, step=0.5, binary_dim=10):
     if mat.ndim == 1:  # If the matrix is a vector
         mat = mat[..., np.newaxis]  # Transform into a matrix
     normalized_mat = (mat - lower_bound) / (higher_bound - lower_bound)
-    if enc_init == False:
-        step_vec = np.random.uniform(step/2, step, binary_dim)
-        dither_vec = np.random.uniform(0, 1, binary_dim)
+    step = np.random.uniform(step/2, step, binary_dim)
 
     enc_input_data = np.repeat(np.zeros(mat.shape), binary_dim, axis=-1)
     for i_binary in range(binary_dim):
         enc_input_data[..., i_binary::binary_dim] = \
-            np.mod((normalized_mat - dither_vec[i_binary]) // step_vec[i_binary], 2) == 0
-    
-    return enc_input_data, step_vec, dither_vec
+            np.mod((normalized_mat - np.random.uniform()) // step[i_binary], 2) == 0
+    return enc_input_data
